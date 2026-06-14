2c220625790bbd7971509eba07aa0ebce57b036c
Byealha <junpll0527@naver.com>
2026-06-14 12:40:57 +0900

    Add intro scene book

--- Assets/Scripts/Intro/IntroInput.cs
+++ Assets/Scripts/Intro/IntroInput.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class IntroInput : MonoBehaviour
 {
     [SerializeField] private IntroSystemManager introSystemManager;
+    [SerializeField] private AutoFlip autoFlip;
 
     private void Update()
     {
         if (FadeManager.Instance != null && FadeManager.Instance.IsFading) return;
 
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            introSystemManager.MoveNextScene();
+            autoFlip.FlipRightPage();
         }
     }
 }
--- Assets/Scripts/Intro/IntroSystemManager.cs
+++ Assets/Scripts/Intro/IntroSystemManager.cs
@@ -1,16 +1,48 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroSystemManager : MonoBehaviour
 {
     [SerializeField] private string nextSceneName = "PlayerRoom";
+    [SerializeField] private Book book;
+    [SerializeField] private int lastPage = 7;
+    [SerializeField] private Image NextSceneBtn;
 
-    public void NextPage()
+    private bool _lastPageVisit = false;
+
+    private void Awake()
     {
-        
+        NextSceneBtn.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (book.currentPage == lastPage && !_lastPageVisit)
+            MoveNextSceneReady();
+    }
+
+    private void MoveNextSceneReady()
+    {
+        _lastPageVisit = true;
+        StartCoroutine(ShowNextSceneBtn());
     }
 
     public void MoveNextScene()
     {
         SceneLoadManager.Instance.LoadScene(nextSceneName);
+    }
+
+    private IEnumerator ShowNextSceneBtn()
+    {
+        NextSceneBtn.color = new Color(1f, 1f, 1f, 0f);
+        NextSceneBtn.gameObject.SetActive(true);
+        float alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += 0.05f;
+            yield return new WaitForSecondsRealtime(0.01f);
+            NextSceneBtn.color = new Color(1f, 1f, 1f, alpha);
+        }
     }
 }
