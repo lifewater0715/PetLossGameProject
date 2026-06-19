e8113f2b8e9ab2c7f0e12436fb0c51c032415bab
Byealha <junpll0527@naver.com>
2026-06-19 18:49:52 +0900

    Edit shampoo mini game ui polishing

--- Assets/Scripts/Minigames/Shampoo/UI/ShampooChargeSliderController.cs
+++ Assets/Scripts/Minigames/Shampoo/UI/ShampooChargeSliderController.cs
@@ -6,6 +6,8 @@ public class ShampooChargeSliderController : MonoBehaviour
     [SerializeField] private ShampooSystemManager shampooSystemManager;
     [SerializeField] private Slider chargeSlider;
     [SerializeField] private float decreaseSpeed = 4f;
+    [SerializeField] private float slowDownStartValue = 0.25f;
+    [SerializeField] private float minDecreaseSpeedMultiplier = 0.005f;
 
     private void Awake()
     {
@@ -36,9 +38,26 @@ public class ShampooChargeSliderController : MonoBehaviour
             return;
         }
 
+        float decreaseStep = GetDecreaseStep();
+
         chargeSlider.normalizedValue = Mathf.MoveTowards(
             chargeSlider.normalizedValue,
             targetValue,
-            Time.deltaTime * decreaseSpeed);
+            decreaseStep);
+    }
+
+    private float GetDecreaseStep()
+    {
+        float speedMultiplier = 1f;
+        float clampedSlowDownStartValue = Mathf.Clamp01(slowDownStartValue);
+        float clampedMinMultiplier = Mathf.Clamp01(minDecreaseSpeedMultiplier);
+
+        if (clampedSlowDownStartValue > 0f && chargeSlider.normalizedValue < clampedSlowDownStartValue)
+        {
+            float slowDownRate = chargeSlider.normalizedValue / clampedSlowDownStartValue;
+            speedMultiplier = Mathf.Lerp(clampedMinMultiplier, 1f, slowDownRate);
+        }
+
+        return Time.deltaTime * decreaseSpeed * speedMultiplier;
     }
 }
